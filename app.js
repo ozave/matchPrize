30beb51950a2ca5ed2d1350a47100b2760e6bcfd
ozav_tom <ozave@yahoo.com>
2015-07-18 04:25:53 +0100

    commit appjs

---
+++ app.js
@@ -0,0 +1,80 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+//require('./models/Leagues');
+//require('./models/FFTeams');
+//require('./models/Players');
+//require('./models/Challenges');
+//require('./models/OneOnOneChallenges');
+//require('./models/ChallengeTeams');
+//require('./models/MyTeams');
+require('./models/Users');
+//require('./models/Fixtures');
+//require('./models/Invite');
+//require('./models/LeagueInvite');
+//require('./models/ChallengeInvite');
+//require('./config/passport');
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+var mongoose = require('mongoose');
+//var passport = require('passport');
+
+
+mongoose.connect('mongodb://localhost/matchprize');
+
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
